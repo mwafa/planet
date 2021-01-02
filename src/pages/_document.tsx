6feb32706583fe9ee2a5ee05a472c8409f80ea23
Muhammad Wafa <wafax.4@gmail.com>
2021-01-02 17:05:00 +0700

    clean setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Head, Html, Main, NextScript } from "next/document"
+
+import { ColorModeScript } from "@chakra-ui/react"
 
 export default class Document extends NextDocument {
   render() {
@@ -7,7 +8,6 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
